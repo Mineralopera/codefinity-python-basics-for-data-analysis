f1a86e7d0a1e96171d7e6a48f72899a9a8cda052
Mineralopera <>
2026-03-14 17:27:01 +0000

    String manipulation

---
+++ section/string_manipulation/main.py
@@ -0,0 +1,9 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+
+# Slicing words of the string 
+dairy1 = grocery_items[:4]
+dairy2 = grocery_items[5:11]
+bakery1 = grocery_items[12:17]
+
+# Concatenating string: 
+print('We have dairy and bakery items:' + " " + dairy1 + ', ' + dairy2 + ', ' + 'and ' + bakery1 + ' ' + 'in aisle 5')
