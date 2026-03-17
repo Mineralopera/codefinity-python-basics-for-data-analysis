faac3361719d5f0e686dd007c26ef420b10cfa19
Mineralopera <>
2026-03-17 15:47:53 +0000

    Boolean Operators

---
+++ section/boolean_operators/main.py
@@ -0,0 +1,9 @@
+# Defining total cost
+total_cost = 25.00
+
+# Creating boolean variable
+discountEligible = total_cost >= 20.00
+
+# Testing result:
+print(discountEligible)
+print(f'Is the purchase eligible for a discount? {discountEligible}')
